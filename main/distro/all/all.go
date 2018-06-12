0db3ff6768a52cb8f747b6947bf1fb11ceaf8cdd
restart77 <aiyigou1221@gmail.com>
2018-06-12 17:05:26 +0800

    Update all.go
    删除部分路由多余模块

--- main/distro/all/all.go
+++ main/distro/all/all.go
@@ -9,10 +9,10 @@ import (
 	_ "v2ray.com/core/app/proxyman/outbound"
 
 	// Default commander and all its services. This is an optional feature.
-	_ "v2ray.com/core/app/commander"
-	_ "v2ray.com/core/app/log/command"
-	_ "v2ray.com/core/app/proxyman/command"
-	_ "v2ray.com/core/app/stats/command"
+// 	_ "v2ray.com/core/app/commander"
+// 	_ "v2ray.com/core/app/log/command"
+// 	_ "v2ray.com/core/app/proxyman/command"
+// 	_ "v2ray.com/core/app/stats/command"
 
 	// Other optional features.
 	_ "v2ray.com/core/app/dns"
@@ -22,31 +22,31 @@ import (
 	_ "v2ray.com/core/app/stats"
 
 	// Inbound and outbound proxies.
-	_ "v2ray.com/core/proxy/blackhole"
+// 	_ "v2ray.com/core/proxy/blackhole"
 	_ "v2ray.com/core/proxy/dokodemo"
 	_ "v2ray.com/core/proxy/freedom"
-	_ "v2ray.com/core/proxy/http"
-	_ "v2ray.com/core/proxy/shadowsocks"
+// 	_ "v2ray.com/core/proxy/http"
+// 	_ "v2ray.com/core/proxy/shadowsocks"
 	_ "v2ray.com/core/proxy/socks"
-	_ "v2ray.com/core/proxy/vmess/inbound"
+// 	_ "v2ray.com/core/proxy/vmess/inbound"
 	_ "v2ray.com/core/proxy/vmess/outbound"
 
 	// Transports
-	_ "v2ray.com/core/transport/internet/domainsocket"
-	_ "v2ray.com/core/transport/internet/http"
-	_ "v2ray.com/core/transport/internet/kcp"
+// 	_ "v2ray.com/core/transport/internet/domainsocket"
+// 	_ "v2ray.com/core/transport/internet/http"
+// 	_ "v2ray.com/core/transport/internet/kcp"
 	_ "v2ray.com/core/transport/internet/tcp"
 	_ "v2ray.com/core/transport/internet/tls"
 	_ "v2ray.com/core/transport/internet/udp"
 	_ "v2ray.com/core/transport/internet/websocket"
 
 	// Transport headers
-	_ "v2ray.com/core/transport/internet/headers/http"
-	_ "v2ray.com/core/transport/internet/headers/noop"
-	_ "v2ray.com/core/transport/internet/headers/srtp"
-	_ "v2ray.com/core/transport/internet/headers/tls"
-	_ "v2ray.com/core/transport/internet/headers/utp"
-	_ "v2ray.com/core/transport/internet/headers/wechat"
+// 	_ "v2ray.com/core/transport/internet/headers/http"
+// 	_ "v2ray.com/core/transport/internet/headers/noop"
+// 	_ "v2ray.com/core/transport/internet/headers/srtp"
+// 	_ "v2ray.com/core/transport/internet/headers/tls"
+// 	_ "v2ray.com/core/transport/internet/headers/utp"
+// 	_ "v2ray.com/core/transport/internet/headers/wechat"
 
 	// JSON config support. Choose only one from the two below.
 	// The following line loads JSON from v2ctl
